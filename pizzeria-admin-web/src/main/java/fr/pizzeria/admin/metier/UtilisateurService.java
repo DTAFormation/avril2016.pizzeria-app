e098c288b09367c71a71bf41ee3852bc3d23d9f4
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-23 11:14:52 +0200

    2 actions
    * Ajout entité Utilisateur dans persistance.xml
    * Création UtilisateurSertvice

---
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/UtilisateurService.java
@@ -0,0 +1,37 @@
+package fr.pizzeria.admin.metier;
+
+import fr.pizzeria.model.Utilisateur;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class UtilisateurService {
+
+  @PersistenceContext protected EntityManager em;
+
+
+  public List<Utilisateur> findAll() {
+    return em.createQuery("select u from Utilisateur u", Utilisateur.class).getResultList();
+  }
+
+  public Utilisateur findOneUtilisateur(String email) {
+    return em.createQuery("select u from Utilisateur u where u.email=:email", Utilisateur.class)
+        .setParameter("email", email)
+        .getSingleResult();
+  }
+
+  public void updateUtilisateur(String email, Utilisateur utilisateurAvecId) {
+    findOneUtilisateur(email); // vérifie qu'une pizza est présente
+    em.merge(utilisateurAvecId);
+  }
+
+  public void saveUtilisateur(Utilisateur utilisateurSansId) {
+    em.persist(utilisateurSansId);
+  }
+
+  public void deleteUtilisateur(String email) {
+    em.remove(findOneUtilisateur(email));
+  }
+}
