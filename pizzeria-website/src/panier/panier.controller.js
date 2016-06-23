202651277cfccfdeb64c26d9db8c8df2945d0819
Olivier LE FLOHIC <olivier.a.leflohic@gmail.com>
2016-06-23 16:45:03 +0200

    branche + ajout suppression élément du panier

--- pizzeria-website/src/panier/panier.controller.js
+++ pizzeria-website/src/panier/panier.controller.js
@@ -7,8 +7,19 @@ export class PanierController {
     // TEMPORAIRE : contenu en dur, remplacer avec données en stockage local
     this.contenu = [
       { 'pizza': new Pizza({'code': 'royale', 'nom': 'Royale', 'prix': 12, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 },
-      { 'pizza': new Pizza({'code': 'marguerita', 'nom': 'Margherita', 'prix': 15, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 3 }
+      { 'pizza': new Pizza({'code': 'marguerita', 'nom': 'Margherita', 'prix': 15, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 3 },
+      { 'pizza': new Pizza({'code': 'savoyarde', 'nom': 'Savoyarde', 'prix': 14, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 4 },
+      { 'pizza': new Pizza({'code': '4fromages', 'nom': '4 fromages', 'prix': 10, 'categorie': 'SANS_VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 }
     ]
+  }
+
+  supprimerPizza (codePizza) {
+    for (var i = 0; i < this.contenu.length; i++) {
+      if (this.contenu[i].pizza.code === codePizza) {
+        this.contenu.splice(i, 1)
+        break
+      }
+    }
   }
 
   // remarque : juste une idée pour le multilangue (ne fonctionne pas)
