af7d4faa8cb97c9017a0c09f234f8325ee3485f5
Valentin Michel <elfindel69@gmail.com>
2016-06-23 15:42:03 +0200

    modifs correction coquilles

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/ClientService.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/ClientService.java
@@ -1,51 +1,48 @@
 package fr.pizzeria.admin.metier;
 
-import fr.pizzeria.model.Client;
-
-import java.util.Date;
 import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import fr.pizzeria.model.Client;
+
 @Stateless
 public class ClientService {
 
-  @PersistenceContext protected EntityManager em;
+	@PersistenceContext
+	protected EntityManager em;
 
-  
+	public List<Client> findAll() {
+		return em.createQuery("select c from Client c where isActive = 1", Client.class).getResultList();
+	}
 
-  public List<Client> findAll() {
-    return em.createQuery("select c from Client c where isActive = 1", Client.class).getResultList();
-  }
+	public Client findOneClient(String email) {
+		return em.createQuery("select c from Client c where c.email=:email and isActive = 1", Client.class)
+				.setParameter("email", email).getSingleResult();
+	}
 
-  public Client findOneClient(String email) {
-    return em.createQuery("select c from Client c where p.email=:email and isActive = 1", Client.class)
-        .setParameter("email", email)
-        .getSingleResult();
-  }
+	public void updateClient(String email, Client clientAvecId) {
+		findOneClient(email); // vérifie qu'un client est présent
+		em.merge(clientAvecId);
+	}
 
-  public void updateClient(String email, Client clientAvecId) {
-	  findOneClient(email); // vérifie qu'un client est présent
-    em.merge(clientAvecId);
-  }
+	public void saveClient(Client clientSansId) {
+		em.persist(clientSansId);
+	}
 
-  public void saveClient(Client clientSansId) {
-    em.persist(clientSansId);
-  }
+	public void deleteClient(String email) {
+		Client c = findOneClient(email);
+		if (c != null) {
+			c.setActive(false);
+			em.merge(c);
+		}
 
-  public void deleteClient(String email) {
-    Client c = findOneClient(email);
-    if(c != null){
-    	c.setActive(false);
-    	em.merge(c);
-    }
-	  
-  }
+	}
 
-public void setEm(EntityManager em2) {
-	this.em = em2;
-	
-	
-}
+	public void setEm(EntityManager em2) {
+		this.em = em2;
+
+	}
 }
--- pizzeria-admin-web/src/test/java/fr/pizzeria/admin/metier/ClientServiceTest.java
+++ pizzeria-admin-web/src/test/java/fr/pizzeria/admin/metier/ClientServiceTest.java
@@ -1,9 +1,8 @@
 package fr.pizzeria.admin.metier;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -14,71 +13,72 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.LoggerFactory;
-import static org.mockito.Mockito.*;
+
 import fr.pizzeria.model.Client;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ClientServiceTest {
 	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ClientServiceTest.class);
-	
-	@Mock 
+
+	@Mock
 	private EntityManager em;
-	
+
 	@Mock
 	private TypedQuery<Client> query;
-	
+
 	private ClientService service;
-	
+
 	@Before
 	public void setUp() {
 		service = new ClientService();
-		service.setEm(em);		
+		service.setEm(em);
 	}
-	
+
 	@Test
-	public void creerClient() {		
-		LOG.info("Etant donne un objet Client");		
-		Client client = new Client("test","test","test@test.fr","1541604","10 av aa","00000000");
-		
+	public void creerClient() {
+		LOG.info("Etant donne un objet Client");
+		Client client = new Client("test", "test", "test@test.fr", "10 av aa", "00000000");
+
 		LOG.info("Lorsque ejb.saveClient(client)");
 		service.saveClient(client);
-		
+
 		LOG.info("Alors 'client' a ete persiste");
 		verify(em).persist(client);
 		LOG.info("FIN");
 	}
-	
+
 	@Test
-	public void supprimerClient() {		
-		LOG.info("Etant donne un objet Client");		
-		Client client = new Client(1,"test","test","test@test.fr","1541604","10 av aa","00000000");
-		when(em.createQuery("select c from Client c where p.email=:email and isActive = 1", Client.class)).thenReturn(query);
+	public void supprimerClient() {
+		LOG.info("Etant donne un objet Client");
+		Client client = new Client(1, "test", "test", "test@test.fr", "10 av aa", "00000000");
+		when(em.createQuery("select c from Client c where c.email=:email and isActive = 1", Client.class))
+				.thenReturn(query);
 		when(query.setParameter("email", "test@test.fr")).thenReturn(query);
 		when(query.getSingleResult()).thenReturn(client);
 		LOG.info("Lorsque ejb.deleteClient(client)");
 		service.deleteClient("test@test.fr");
-		
+
 		LOG.info("Alors 'client' a ete modifie");
 		verify(em).merge(client);
-		
+
 		LOG.info("FIN");
 	}
-	
+
 	@Test
-	public void supprimerClientVerifModifIsActive() {		
-		LOG.info("Etant donne un objet Client");		
-		Client client = new Client(1,"test","test","test@test.fr","1541604","10 av aa","00000000");
-		when(em.createQuery("select c from Client c where p.email=:email and isActive = 1", Client.class)).thenReturn(query);
+	public void supprimerClientVerifModifIsActive() {
+		LOG.info("Etant donne un objet Client");
+		Client client = new Client(1, "test", "test", "test@test.fr", "10 av aa", "00000000");
+		when(em.createQuery("select c from Client c where c.email=:email and isActive = 1", Client.class))
+				.thenReturn(query);
 		when(query.setParameter("email", "test@test.fr")).thenReturn(query);
 		when(query.getSingleResult()).thenReturn(client);
 		LOG.info("Lorsque ejb.deleteClient(client)");
 		service.deleteClient("test@test.fr");
-		
+
 		LOG.info("Alors 'client' a ete modifie et is Active est modifié à false");
 		verify(em).merge(client);
 		assertFalse(client.isActive());
 		LOG.info("FIN");
 	}
-	
 
 }
