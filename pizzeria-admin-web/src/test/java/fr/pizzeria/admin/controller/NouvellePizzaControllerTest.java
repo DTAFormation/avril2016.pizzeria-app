d1d5c66f8351d31b24fe86e7638d6817a52f09a7
Albin Lemattre <lemattreal@gmail.com>
2016-06-24 17:41:35 +0200

    début des test

---
+++ pizzeria-admin-web/src/test/java/fr/pizzeria/admin/controller/NouvellePizzaControllerTest.java
@@ -0,0 +1,22 @@
+package fr.pizzeria.admin.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NouvellePizzaControllerTest {
+
+	@Test
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
