8beced75b22a7d71375258ad9277f58d83b5603c
Olivier LE FLOHIC <olivier.a.leflohic@gmail.com>
2016-06-24 15:11:33 +0200

    mis le code debug en commentaire

--- pizzeria-website/src/panier/panier.controller.js
+++ pizzeria-website/src/panier/panier.controller.js
@@ -1,5 +1,3 @@
-import { Pizza } from '../shared/model/pizza'
-
 export class PanierController {
 
   constructor (PanierService) {
@@ -7,16 +5,17 @@ export class PanierController {
     this.PanierService = PanierService
     this.contenu = this.PanierService.$localStorage.panier
 
-    // DEBUG - Décommenter cette section pour activer - Contenu en dur défini lors de chaque rechargement de page.
+    // DEBUG - Décommenter/recommenter cette section pour activer/désactiver
+    // Contenu en dur du panier défini lors de chaque rechargement de page, et qui écrase le contenu en stockage.
     // Utile pour obtenir un panier pré-rempli pour tester l'affichage et les opérations nécessitant un panier déjà rempli.
     // -----------------------------
-    this.contenu = {
-      '1': { 'pizza': new Pizza({'id': 1, 'code': 'royale', 'nom': 'Royale', 'prix': 12, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 },
-      '2': { 'pizza': new Pizza({'id': 2, 'code': 'marguerita', 'nom': 'Margherita', 'prix': 15, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 3 },
-      '3': { 'pizza': new Pizza({'id': 3, 'code': 'savoyarde', 'nom': 'Savoyarde', 'prix': 14, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 4 },
-      '4': { 'pizza': new Pizza({'id': 4, 'code': '4fromages', 'nom': '4 fromages', 'prix': 10, 'categorie': 'SANS_VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 }
-    }
-    this.PanierService.$localStorage.panier = this.contenu
+    // this.contenu = {
+    //   '1': { 'pizza': new Pizza({'id': 1, 'code': 'royale', 'nom': 'Royale', 'prix': 12, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 },
+    //   '2': { 'pizza': new Pizza({'id': 2, 'code': 'marguerita', 'nom': 'Margherita', 'prix': 15, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 3 },
+    //   '3': { 'pizza': new Pizza({'id': 3, 'code': 'savoyarde', 'nom': 'Savoyarde', 'prix': 14, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 4 },
+    //   '4': { 'pizza': new Pizza({'id': 4, 'code': '4fromages', 'nom': '4 fromages', 'prix': 10, 'categorie': 'SANS_VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 }
+    // }
+    // this.PanierService.$localStorage.panier = this.contenu
     // -----------------------------
     // FIN DEBUG
   }
