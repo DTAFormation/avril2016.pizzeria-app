eb9a2da40d337f558518d05f894449f60a03198d
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-24 14:43:35 +0200

    Redéfinition de la méthode equals()

--- pizzeria-model/src/main/java/fr/pizzeria/model/Utilisateur.java
+++ pizzeria-model/src/main/java/fr/pizzeria/model/Utilisateur.java
@@ -73,6 +73,53 @@ public class Utilisateur {
 	public void setMotDePasse(String motDePasse) {
 		this.motDePasse = motDePasse;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((motDePasse == null) ? 0 : motDePasse.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Utilisateur other = (Utilisateur) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (motDePasse == null) {
+			if (other.motDePasse != null)
+				return false;
+		} else if (!motDePasse.equals(other.motDePasse))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (prenom == null) {
+			if (other.prenom != null)
+				return false;
+		} else if (!prenom.equals(other.prenom))
+			return false;
+		return true;
+	}
 
 	
 	
