021c171800c39236b3e9dee27498d961a28660a5
samuel perchaud <perchaud.samuel@gmail.com>
2016-06-24 15:21:53 +0200

    commit de fin de l'implémentation du backend connection avec mise a jour des imports

--- pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/resource/ClientResource.java
+++ pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/resource/ClientResource.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.pizzeria.model.Client;
@@ -29,5 +28,4 @@ public class ClientResource {
 		return clientDao.save(client);
 	}
 	
-
 }
