8ba578576f63607d4e48efc234ca1ea05d57d811
KaadArloon <jphdidier@gmail.com>
2016-06-27 15:41:50 +0200

    Informations de la pizzeria
    Lien d'accès dans le footer et dans la barre de navigation
    
    Ajout des données en dur dans la page about.html

--- pizzeria-website/src/navbar/navbar.component.js
+++ pizzeria-website/src/navbar/navbar.component.js
@@ -11,7 +11,7 @@ export const navbarComponent = {
       <ul class="nav navbar-nav">
         <li ng-class="{active : $ctrl.isActive('/home')}"><a href="#/">Accueil</a></li>
         <li ng-class="{active : $ctrl.isActive('/pizzas')}" role="presentation"><a href="#/pizzas">Nos Pizzas</a></li>
-        <li ng-class="{active : $ctrl.isActive('/commandes')}" role="presentation"><a href="#/commandes/{{ $ctrl.$localStorage.client.id }}">Vos Commandes</a></li>
+        <li ng-class="{active : $ctrl.isActive('/commandes')}" role="presentation"><a href="#/commandes/{{ $ctrl.$localStorage.client.id }}" role="presentation">Vos Commandes</a></li>
         <li ng-class="{active : $ctrl.isActive('/about')}" role="presentation"><a href="#/about">A propos</a></li>
         <li ng-class="{active : $ctrl.isActive('/inscription')}" role="presentation"><a href="#/inscription">Inscription</a></li>
         <li ng-if="!$ctrl.logged" ng-class="{active : $ctrl.isActive('/connexion')}" role="presentation"><a href="#/connexion">Connexion</a></li>
