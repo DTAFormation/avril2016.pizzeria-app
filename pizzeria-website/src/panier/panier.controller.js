1712f93f412731ae88d8cc3034e5b6ec935ef0a7
Olivier LE FLOHIC <olivier.a.leflohic@gmail.com>
2016-06-27 12:03:13 +0200

    désactivation de code utilisé normalement pour le debug

--- pizzeria-website/src/panier/panier.controller.js
+++ pizzeria-website/src/panier/panier.controller.js
@@ -1,4 +1,4 @@
-import { Pizza } from '../shared/model/pizza'
+// import { Pizza } from '../shared/model/pizza'
 
 export class PanierController {
 
@@ -11,13 +11,13 @@ export class PanierController {
     // Contenu en dur du panier défini lors de chaque rechargement de page, et qui écrase le contenu en stockage.
     // Utile pour obtenir un panier pré-rempli pour tester l'affichage et les opérations nécessitant un panier déjà rempli.
     // -----------------------------
-    this.contenu = {
-      '1': { 'pizza': new Pizza({'id': 1, 'code': 'royale', 'nom': 'Royale', 'prix': 12, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 },
-      '2': { 'pizza': new Pizza({'id': 2, 'code': 'marguerita', 'nom': 'Margherita', 'prix': 15, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 3 },
-      '3': { 'pizza': new Pizza({'id': 3, 'code': 'savoyarde', 'nom': 'Savoyarde', 'prix': 14, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 4 },
-      '4': { 'pizza': new Pizza({'id': 4, 'code': '4fromages', 'nom': '4 fromages', 'prix': 10, 'categorie': 'SANS_VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 }
-    }
-    this.PanierService.$localStorage.panier = this.contenu
+    // this.contenu = {
+    //   '1': { 'pizza': new Pizza({'id': 1, 'code': 'royale', 'nom': 'Royale', 'prix': 12, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 },
+    //   '2': { 'pizza': new Pizza({'id': 2, 'code': 'marguerita', 'nom': 'Margherita', 'prix': 15, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 3 },
+    //   '3': { 'pizza': new Pizza({'id': 3, 'code': 'savoyarde', 'nom': 'Savoyarde', 'prix': 14, 'categorie': 'VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 4 },
+    //   '4': { 'pizza': new Pizza({'id': 4, 'code': '4fromages', 'nom': '4 fromages', 'prix': 10, 'categorie': 'SANS_VIANDE', 'urlImage': 'http://placehold.it/150x150'}), 'quantite': 1 }
+    // }
+    // this.PanierService.$localStorage.panier = this.contenu
     // -----------------------------
     // FIN DEBUG
   }
