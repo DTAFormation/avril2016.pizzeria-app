22decba6be16522d0cab79af0c51604510d8361a
Albin Lemattre <lemattreal@gmail.com>
2016-06-27 14:21:54 +0200

    déplacement de l'encodage md5 du controller vers le service #3

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/UtilisateurService.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/UtilisateurService.java
@@ -1,6 +1,9 @@
 package fr.pizzeria.admin.metier;
 
 import fr.pizzeria.model.Utilisateur;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -38,5 +41,34 @@ public class UtilisateurService {
 
   public void deleteUtilisateur(String email) {
     em.remove(findOneUtilisateur(email));
+  }
+  
+  public String encode(String password)
+  {
+      byte[] uniqueKey = password.getBytes();
+      byte[] hash      = null;
+
+      try
+      {
+          hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
+      }
+      catch (NoSuchAlgorithmException e)
+      {
+          throw new Error("No MD5 support in this VM.");
+      }
+
+      StringBuilder hashString = new StringBuilder();
+      for (int i = 0; i < hash.length; i++)
+      {
+          String hex = Integer.toHexString(hash[i]);
+          if (hex.length() == 1)
+          {
+              hashString.append('0');
+              hashString.append(hex.charAt(hex.length() - 1));
+          }
+          else
+              hashString.append(hex.substring(hex.length() - 2));
+      }
+      return hashString.toString();
   }
 }
--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/utilisateur/EditerUtilisateurController.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/utilisateur/EditerUtilisateurController.java
@@ -85,7 +85,7 @@ public class EditerUtilisateurController extends HttpServlet {
     	    
         } else {
         	
-        	String mdpEncode = encode(motDePasse);
+        	String mdpEncode = utilisateurService.encode(motDePasse);
         	
         	Utilisateur utilisateurAvecId = new Utilisateur(new Integer(id), nom, prenom, email, mdpEncode);
 
@@ -97,34 +97,5 @@ public class EditerUtilisateurController extends HttpServlet {
 
     protected boolean isBlank(String param) {
         return param == null || param.isEmpty();
-    }
-
-    protected static String encode(String password)
-    {
-        byte[] uniqueKey = password.getBytes();
-        byte[] hash      = null;
-
-        try
-        {
-            hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            throw new Error("No MD5 support in this VM.");
-        }
-
-        StringBuilder hashString = new StringBuilder();
-        for (int i = 0; i < hash.length; i++)
-        {
-            String hex = Integer.toHexString(hash[i]);
-            if (hex.length() == 1)
-            {
-                hashString.append('0');
-                hashString.append(hex.charAt(hex.length() - 1));
-            }
-            else
-                hashString.append(hex.substring(hex.length() - 2));
-        }
-        return hashString.toString();
     }
 }
