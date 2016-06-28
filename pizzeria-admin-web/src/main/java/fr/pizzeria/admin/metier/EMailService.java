91c3ec9d2d642775df04e6156a1c846241e8eb15
Anthony Auzizeau <aatonyoz@gmail.com>
2016-06-28 10:02:01 +0200

    commit mail #9

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/EMailService.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/EMailService.java
@@ -1,4 +1,11 @@
 package fr.pizzeria.admin.metier;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -6,34 +13,53 @@ import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
- 
+
 @Stateless
 public class EMailService {
- 
-    @Resource(name = "java:/Email")
-    private Session session;
- 
-    public void send(String addresses, String topic, String textMessage) {
- 
-        try {
- 
-            Message message = new MimeMessage(session);
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(addresses));
-            message.setSubject(topic);
-            message.setText(textMessage);
- 
-            Transport.send(message);
- 
-        } catch (MessagingException e) {
-            Logger.getLogger(EMailService.class.getName()).log(Level.WARNING, "Cannot send mail", e);
-        }
-    }
-    
 
- 
-    
+	@Resource(name = "java:/Email")
+	private Session session;
+
+	public void send(String addresses, String topic, String textMessage) {
+
+		Session session = Session.getDefaultInstance(fMailServerConfig, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("username", "password");
+			}
+		});
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(addresses));
+			message.setSubject(topic);
+			message.setText(textMessage);
+
+			Transport.send(message);
+
+		} catch (MessagingException e) {
+			Logger.getLogger(EMailService.class.getName()).log(Level.WARNING, "Cannot send mail", e);
+		}
+
+	}
+	 private static Properties fMailServerConfig = new Properties();
+
+	  static {
+	    fetchConfig();
+	  }
+	  
+	private static void fetchConfig() {
+		// This file contains the javax.mail config properties mentioned above.
+		Path path = Paths.get("email");
+		try (InputStream input = Files.newInputStream(path)) {
+			fMailServerConfig.load(input);
+		} catch (IOException ex) {
+			System.err.println("Cannot open and load mail server properties file.");
+		}
+	}
+
 }
