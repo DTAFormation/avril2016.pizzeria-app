713c9c7ba132668a1e2784f553dfd1d970fc36ab
Olivier LE FLOHIC <olivier.a.leflohic@gmail.com>
2016-07-01 16:17:21 +0200

    màj du modèle Livreur avec code livreur + ajout tests

--- pizzeria-model/src/main/java/fr/pizzeria/model/Livreur.java
+++ pizzeria-model/src/main/java/fr/pizzeria/model/Livreur.java
@@ -1,15 +1,21 @@
 package fr.pizzeria.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.apache.commons.lang3.StringUtils;
+
 @Entity
 public class Livreur {
 	
-	public Livreur()
-	{this.actif = true; }
+	public static final String CODE_LIVREUR_PAR_DEFAUT = "DEFAUT"; 
+	
+	public Livreur() {
+		this.actif = true;
+	}
 	
 	public Livreur(String nom, String prenom) {
 		super();
@@ -20,14 +26,50 @@ public class Livreur {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	@Column(unique=true)
+	private String code;
 	private String nom;
 	private String prenom;
 	private Boolean actif;
+	
+	
+	public String genererCodeBrut () {
+		// supprimer les accents
+		String nomStr = StringUtils.stripAccents(nom);
+		String prenomStr = StringUtils.stripAccents(prenom);
+		
+		// supprimer tout ce qui n'est pas une lettre
+		nomStr = StringUtils.removePattern(nomStr, "[^A-Za-z]");
+		prenomStr = StringUtils.removePattern(prenomStr, "[^A-Za-z]");
+		
+		// prendre les 3 premiers caractères, ou tous les caractères s'il y en a moins
+		nomStr = nomStr.substring(0, (nomStr.length() > 2) ? 3 : nomStr.length());
+		prenomStr = prenomStr.substring(0, (prenomStr.length() > 2) ? 3 : prenomStr.length());
+		
+		if ((nomStr + prenomStr).length() == 0) {
+			// la chaîne en sortie serait vide : retourner le code par défaut
+			return CODE_LIVREUR_PAR_DEFAUT;
+		} else {
+			// concaténer le nom et prénom, passer le tout en majuscules
+			return (nomStr + prenomStr).toUpperCase();
+		}
+		
+	}
+	
+	
+	
+
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
 	}
 	public String getNom() {
 		return nom;
@@ -53,7 +95,7 @@ public class Livreur {
 
 	@Override
 	public String toString() {
-		return prenom+" "+nom;
+		return prenom + " " + nom;
 	}
 
 	
