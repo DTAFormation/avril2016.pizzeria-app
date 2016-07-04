4d93dd16f558b27b29a1e9b93274f6749cbde54f
Kevin Mokili <kevin.mokili@gmail.com>
2016-07-04 16:13:38 +0200

    Ajout dépendance karma-verbose-reporter #54

--- pizzeria-website/karma.conf.js
+++ pizzeria-website/karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
 
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
+    // possible values: 'dots', 'progress', 'verbose'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
--- pizzeria-website/test/navbar.controller.spec.js
+++ pizzeria-website/test/navbar.controller.spec.js
@@ -4,18 +4,26 @@ describe('Test du NavbarController', function () {
   var rootScope
   var ctrl
   var localStorage
-  var componentController
+  /*, $navbarController*/
 
-  beforeEach(angular.mock.inject(function ($rootScope, $controller, $localStorage, $componentController) {
+  beforeEach(angular.mock.inject(function ($rootScope, $controller, $location, $localStorage) {
     const scope = $rootScope.$new()
-    rootScope = $rootScope
-    ctrl = $controller
+    rootScope = scope
     localStorage = $localStorage
-    componentController = $componentController
+    ctrl = $controller('NavbarController', {$location: 'http://localhost:9966/#/home', localStorage: null, $scope: scope})
+    // navbarController = $navbarController
   }))
 
   it('should confirm that the view is active', function () {
+    let navbarCtrl = ctrl
+    // navbarCtrl.localStorage = null
+    // navbarCtrl.location = 'http://localhost:9966/#/home'
+    // navbarCtrl.rootScope = rootScope
 
+    var viewLocation = '/home'
+    var path = navbarCtrl.location.path()
+    expect(path).toBeDefined()
+    expect(path).toEqual(viewLocation)
   })
 
   it('should display links associated with account management', function () {
@@ -25,5 +33,4 @@ describe('Test du NavbarController', function () {
   it('should display the default navbar', function () {
 
   })
-
 })
