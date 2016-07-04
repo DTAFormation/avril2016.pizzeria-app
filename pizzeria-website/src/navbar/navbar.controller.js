cb4564fd72a53406e3c461bfd45cde68e8b589dd
Kevin Mokili <kevin.mokili@gmail.com>
2016-07-04 10:36:04 +0200

    Retrait commentaire #54

--- pizzeria-website/src/navbar/navbar.controller.js
+++ pizzeria-website/src/navbar/navbar.controller.js
@@ -2,9 +2,6 @@ export class NavbarController {
   constructor ($location, $localStorage, $rootScope) {
     this.$localStorage = $localStorage
     this.isActive = function (viewLocation) {
-      // console.log('viewLocation is ', viewLocation, '\n$location is ', $location,
-      //   '\n$location path is ', $location.path(), '\nComparison result is ', $location.path().indexOf(viewLocation))
-      
       // Recherche l'url de viewLocation dans $location.path()
       // return !$location.path().indexOf(viewLocation)
       return $location.path().indexOf(viewLocation) !== -1
