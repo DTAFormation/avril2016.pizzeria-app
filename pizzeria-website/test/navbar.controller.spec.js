a85ea839d1f896744fb8fb82d65a4671a19a6bd4
Kevin Mokili <kevin.mokili@gmail.com>
2016-07-06 11:21:36 +0200

    tests à peaufiner #54

--- pizzeria-website/test/navbar.controller.spec.js
+++ pizzeria-website/test/navbar.controller.spec.js
@@ -1,12 +1,17 @@
 import { NavbarController } from '../src/navbar/navbar.controller'
+import { Client } from '../src/shared/model/client'
 
 describe('Test du NavbarController', function () {
   var ctrl
   var navbar
+  var http
+  var localStorage
+  var ctrlConnexion
+  var ctrlDeconnexion
 
   beforeEach(angular.mock.module('pizzeria-website'))
 
-  beforeEach(angular.mock.inject(function ($rootScope, $componentController, $location) {
+  beforeEach(angular.mock.inject(function ($rootScope, $componentController, $location, $controller, $httpBackend, $localStorage) {
     const scope = $rootScope.$new()
     navbar = {
       controller: NavbarController,
@@ -31,6 +36,10 @@ describe('Test du NavbarController', function () {
       `
     }
     ctrl = $componentController('navbarComponent', {$location, $scope: scope}, {navbar: navbar})
+    ctrlConnexion = $controller('ConnexionController', {$scope: scope})
+    ctrlDeconnexion = $controller('DeconnexionController', {$scope: scope})
+    http = $httpBackend
+    localStorage = $localStorage
   }))
 
   // Test en cours d'écriture
@@ -68,6 +77,7 @@ describe('Test du NavbarController', function () {
   })
 
   it('should display links associated with account management after login (connexion)', function () {
+    // Définition 
     let htmlLoggedOut = `
       <nav class="navbar navbar-default">
         <div class="container-fluid">
@@ -85,7 +95,7 @@ describe('Test du NavbarController', function () {
       </nav>
       `
 
-      let htmlLoggedIn = `
+    let htmlLoggedIn = `
       <nav class="navbar navbar-default">
         <div class="container-fluid">
           <div class="navbar-header">
@@ -97,16 +107,96 @@ describe('Test du NavbarController', function () {
             <li ng-if="$ctrl.logged" ng-class="{active : $ctrl.isActive('/commandes')}" role="presentation"><a href="#/commandes/{{ $ctrl.$localStorage.client.id }}">Vos Commandes</a></li>
             <li ng-class="{active : $ctrl.isActive('/about')}" role="presentation"><a href="#/about">A propos</a></li>
             <li ng-if="$ctrl.logged"  ng-class="{active : $ctrl.isActive('/modificationDonneesCompte')}" role="presentation"><a href="#/modificationDonneesCompte">Mon compte</a></li>
-            <li ng-if="!$ctrl.logged" ng-class="{active : $ctrl.isActive('/inscription')}" role="presentation"><a href="#/inscription">Inscription</a></li>
-            <li ng-if="!$ctrl.logged" ng-class="{active : $ctrl.isActive('/connexion')}" role="presentation"><a href="#/connexion">Connexion</a></li>
             <li ng-if="$ctrl.logged"  ng-class="{active : $ctrl.isActive('/deconnexion')}" role="presentation"><a href="#/deconnexion">Se déconnecter</a></li>
           </ul>
         </div>
       </nav>
       `
+    ctrl.navbar.template = htmlLoggedOut
+
+    // Connexion au site
+    localStorage.client = null
+    expect(localStorage.client).toEqual(null)
+
+    let client = new Client({
+      id: 1,
+      nom: 'nom',
+      prenom: 'prenom',
+      email: 'email',
+      motDePasse: 'motDePasse'
+    })
+
+    http.when('POST', 'http://localhost:8080/login').respond(client)
+    // Vérification de la connexion
+    ctrlConnexion.connexionClient({$invalid: false, email: 'email', motDePasse: 'motDePasse'})
+      .then(result => expect(localStorage.client).toEqual(client))
+/*
+    console.log('*** Local Storage ****')
+    console.log('Client : ', localStorage.client)
+    console.log('*** Fin ****')
+
+    console.log(ctrl.logged)
+*/
+    ctrl.logged = true
+    expect(ctrl.logged).toEqual(true)
+    ctrl.navbar.template = htmlLoggedIn
+
+
+    // Vérification de la mise à jour du template
+    expect(ctrl.navbar.template).toBe(htmlLoggedIn)
   })
 
   it('should display the default navbar after logout', function () {
+    let htmlLoggedOut = `
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#/">DTA Pizzeria</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li ng-class="{active : $ctrl.isActive('/home')}"><a href="#/">Accueil</a></li>
+            <li ng-class="{active : $ctrl.isActive('/pizzas')}" role="presentation"><a href="#/pizzas">Nos Pizzas</a></li>
+            <li ng-class="{active : $ctrl.isActive('/about')}" role="presentation"><a href="#/about">A propos</a></li>
+            <li ng-if="!$ctrl.logged" ng-class="{active : $ctrl.isActive('/inscription')}" role="presentation"><a href="#/inscription">Inscription</a></li>
+            <li ng-if="!$ctrl.logged" ng-class="{active : $ctrl.isActive('/connexion')}" role="presentation"><a href="#/connexion">Connexion</a></li>
+          </ul>
+        </div>
+      </nav>
+      `
 
+    let htmlLoggedIn = `
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#/">DTA Pizzeria</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li ng-class="{active : $ctrl.isActive('/home')}"><a href="#/">Accueil</a></li>
+            <li ng-class="{active : $ctrl.isActive('/pizzas')}" role="presentation"><a href="#/pizzas">Nos Pizzas</a></li>
+            <li ng-if="$ctrl.logged" ng-class="{active : $ctrl.isActive('/commandes')}" role="presentation"><a href="#/commandes/{{ $ctrl.$localStorage.client.id }}">Vos Commandes</a></li>
+            <li ng-class="{active : $ctrl.isActive('/about')}" role="presentation"><a href="#/about">A propos</a></li>
+            <li ng-if="$ctrl.logged"  ng-class="{active : $ctrl.isActive('/modificationDonneesCompte')}" role="presentation"><a href="#/modificationDonneesCompte">Mon compte</a></li>
+            <li ng-if="$ctrl.logged"  ng-class="{active : $ctrl.isActive('/deconnexion')}" role="presentation"><a href="#/deconnexion">Se déconnecter</a></li>
+          </ul>
+        </div>
+      </nav>
+      `
+    ctrl.navbar.template = htmlLoggedIn
+    // Déconnexion du site
+    let client = new Client({
+      id: 1,
+      nom: 'nom',
+      prenom: 'prenom',
+      email: 'email',
+      motDePasse: 'motDePasse'
+    })
+    localStorage.client = client
+    expect(localStorage.client).toEqual(client)
+    // ctrlDeconnexion = $controller('DeconnexionController', {$scope: scope})
+    expect(localStorage.client).toEqual(null)
+
+    // Vérification de la déconnexion
+
+    // Vérification de la mise à jour du template
   })
 })
